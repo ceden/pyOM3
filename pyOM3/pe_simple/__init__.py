6b077efb333de4f69fd6fc6241d13e0a19e35183
ceden <carsten.eden@uni-hamburg.de>
2026-02-26 12:53:41 +0100

    Add files via upload

--- pyOM3/pe_simple/__init__.py
+++ pyOM3/pe_simple/__init__.py
@@ -1,1 +1,27 @@
+"""
+ Setup of the simple PE model and base class.
+ 
+ Contains only a dummy function loading all parameters 
+ given on initialisation.
+ 
+"""
+import pyOM3     as OM
+from   functools import partial
+import numpy     as np__
+
+
+class setup():
+
+    
+      def __init__(self,parameter = {},n_pes_x = 1,n_pes_y = 1):   
+         """  
+         Add parameter to instance, dictionary parameter contains 
+         various model parameters. No checks.
+         """                     
+         for key in parameter: setattr(self,key,parameter[key])  
+         return
+
+    
+   
+ 
 
